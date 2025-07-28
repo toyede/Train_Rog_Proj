bdfa5870079a79abfdcd17e44f9a3a410d7adb9e
Jaehoon Kim <cijang74@naver.com>
2025-07-28 21:39:31 +0900

    작은 오브젝트 스폰 로직 수정
    -margin 기능 추가
    -종류 선택 로직 수정
    -겹치는 것 판단할 때 Mesh 기준으로 판단하도록 수정

--- Source/Train_Rog_Proj/BackGroundObjectBase.h
+++ Source/Train_Rog_Proj/BackGroundObjectBase.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 #include "Components/SceneComponent.h"
 #include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
 #include "BackGroundObjectBase.generated.h"
 
 UCLASS()
@@ -19,16 +21,29 @@ public:
     UFUNCTION(BlueprintCallable) // UFUNCTION: 블루프린트에서 SetObjectLocation 함수를 노드로 사용할 수 있게 함
 	void SetObjectLocation(const FVector& NewLocation);
 
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category="Measurement")
+	// 타일 길이 저장용 변수
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category="Tile")
     float MeasuredLength = 0.f;
 
 	// 몇개정도 스폰 예정?
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SmallObjects")
-	int NumSpawn = 2;
+	int32 NumSpawn = 10;
 
 	// 스폰할 소품 종류 리스트
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SmallObjects")
 	TArray<TSubclassOf<AActor>> SmallObjects;
+
+	// 배치 금지 영역을 정의하는 씬 컴포넌트 배열
+    UPROPERTY(VisibleAnywhere, Category = "Setup")
+    TArray<UStaticMeshComponent*> ExclusionZones;
+
+	// 소품 간 및 금지 영역과의 최소 거리 (마진)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning", meta = (ClampMin = "0.0"))
+    float SpawnMargin = 50.0f;
+
+	// 최대 위치 탐색 시도 횟수
+    UPROPERTY(EditAnywhere, Category = "Spawning", meta = (ClampMin = "1"))
+    int32 MaxSpawnAttempts = 20;
 
 protected:
 	virtual void BeginPlay() override;
@@ -40,4 +55,13 @@ public:
 
 	// 전체 영역에 대해 소품 스폰을 자동 분배하는 메서드
     void SpawnObjectsInAllAreas();
+
+	// 이미 스폰된 소품 위치 저장용 배열
+    TArray<FVector> SpawnedLocations;
+
+	// 소품을 스폰해도 되는 위치인지 확인하는 함수
+	bool IsSpawnLocationValid(const FVector& Candidate);
+
+	// 소품을 스폰하는 함수
+	void SpawnSmallBackGrounds();
 };
