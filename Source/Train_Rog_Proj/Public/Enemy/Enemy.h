313a98d48d891d44b15c6f94387133205fb94463
Jokkilook <132244971+Jokkilook@users.noreply.github.com>
2025-09-21 22:54:29 +0900

    고릴라, 나방 공격 뼈대 구현
    -두꺼비 애니메이션 교체
    -고릴라 돌 떤지기
    -나방 콩 떤지기

--- Source/Train_Rog_Proj/Public/Enemy/Enemy.h
+++ Source/Train_Rog_Proj/Public/Enemy/Enemy.h
@@ -66,7 +66,7 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	//공격 함수 - C++과 블루프린트 모두에서 구현 가능
-	UFUNCTION(BlueprintNativeEvent, Category = Attack)
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = Attack)
 	void Attack();
 	virtual void Attack_Implementation();
 
